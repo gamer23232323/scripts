2dfac44f9c9edc0ab3df789ec22e836775c57c86
gamer23232323 <70067072+gamer23232323@users.noreply.github.com>
2021-08-21 07:40:54 +0300

    Create Prison.lua

---
+++ Prison.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/PrisonLife'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/gamer23232323/scripts/main/PrisonLife.lua'),true))()
